1e2d96327de68252aedb5d654267584a56b6cf2c
poirazis <poirazis@gmail.com>
2023-05-18 14:47:04 +0300

    Version 1.0.0

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,12 +1,41 @@
 <script>
-  import { getContext } from "svelte"
+  import { getContext, setContext } from "svelte"
+  import { writable } from "svelte/store"
 
-  export let text
+  export let direction
+  export let size
+  export let quiet
+  export let iconOnly
+  export let compact
 
   const { styleable } = getContext("sdk")
   const component = getContext("component")
+  const buttonGroupStore = writable({ "size" : size, "quiet": quiet, "iconOnly" : iconOnly });
+
+  $: $buttonGroupStore.size = size
+  $: $buttonGroupStore.quiet = quiet
+  $: $buttonGroupStore.iconOnly = iconOnly
+
+  setContext( "super-action-button-group", buttonGroupStore )
 </script>
 
-<div use:styleable={$component.styles}>
-  This is a custom component. The text setting is: {text}.
+<div 
+  class="spectrum-ActionGroup spectrum-ActionGroup--size{size}" 
+  class:spectrum-ActionGroup--vertical={direction=="column"}
+  class:spectrum-ActionGroup--quiet={quiet}
+  class:spectrum-ActionGroup--compact={compact}
+  use:styleable={$component.styles}
+  >
+  <slot />
 </div>
+
+<style>
+  .spectrum-ActionGroup {
+    margin-top: 0px !important;
+    gap: var(--spectrum-global-dimension-size-100);
+  }
+
+  .spectrum-ActionGroup--compact{
+    gap: 0px;
+  }
+</style>
