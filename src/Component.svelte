54ef64a59fc6a7faef46aa50fa289828ce0c1b9f
poirazis <poirazis@gmail.com>
2023-11-27 20:36:55 +0200

    Rename to SuperToolbar

--- src/Component.svelte
+++ src/Component.svelte
@@ -8,7 +8,10 @@
   export let iconOnly
   export let compact
 
-  const { styleable } = getContext("sdk")
+  export let buttonType
+  export let buttons
+
+  const { styleable, Block, BlockComponent } = getContext("sdk")
   const component = getContext("component")
   const buttonGroupStore = writable({ "size" : size, "quiet": quiet, "iconOnly" : iconOnly });
 
@@ -16,18 +19,39 @@
   $: $buttonGroupStore.quiet = quiet
   $: $buttonGroupStore.iconOnly = iconOnly
 
+  const getButtonProps = ( btn ) => {
+    return {
+      size: "M",
+      text: btn.text
+    }
+  }
+
   setContext( "super-action-button-group", buttonGroupStore )
+
+  $: console.log(buttons)
 </script>
 
-<div 
+
+<Block>
+  <div 
   class="spectrum-ActionGroup spectrum-ActionGroup--size{size}" 
   class:spectrum-ActionGroup--vertical={direction=="column"}
   class:spectrum-ActionGroup--quiet={quiet}
   class:spectrum-ActionGroup--compact={compact}
   use:styleable={$component.styles}
   >
-  <slot />
-</div>
+    {#each buttons as button, idx }
+      <BlockComponent
+        type={  buttonType == "action" ? "plugin/bb-coomponent-SuperActionButton" : "button"}
+        props={getButtonProps(button)}
+        order={idx}
+        interactive
+        name="SuperButton"
+      />
+    {/each}
+    <slot />
+  </div>
+</Block>
 
 <style>
   .spectrum-ActionGroup {
